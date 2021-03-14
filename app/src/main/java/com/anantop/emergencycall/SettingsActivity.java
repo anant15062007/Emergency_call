bbb0c7a13e962207e6cf6528b38a51d7df36385f
Anant Jain <anant15062007@gmail.com>
2021-03-15 01:33:31 +0530

    Deleted unwanted things_again......

--- app/src/main/java/com/anantop/emergencycall/SettingsActivity.java
+++ app/src/main/java/com/anantop/emergencycall/SettingsActivity.java
@@ -1,18 +1,8 @@
 package com.anantop.emergencycall;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceFragmentCompat;
-
-import java.text.BreakIterator;
-import java.util.Random;
 
 public class SettingsActivity extends AppCompatActivity
 
