3272829df09fe2ad276efc37c7c27b3a48a1f85c
Anant Jain <anant15062007@gmail.com>
2021-03-19 17:37:54 +0530

    Without any problem successfully sending address

--- app/src/main/java/com/anantop/emergencycall/MainActivity.java
+++ app/src/main/java/com/anantop/emergencycall/MainActivity.java
@@ -12,40 +12,39 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
+import java.text.BreakIterator;
 import java.util.List;
 import java.util.Locale;
 
-
 public class MainActivity extends AppCompatActivity implements LocationListener {
     Button button3 = null;
+    private GoogleMap mMap;
     private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private final long MIN_TIME = 1000;
+    private final long MIN_DIST = 5;
+
+    private LatLng latLng;
     String ph_no;
-    ////private Handler GeocoderHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -133,7 +132,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             @Override
             public boolean onLongClick(View v) {
                 SharedPreferences sharedPreferences = getSharedPreferences("fileNameString", MODE_PRIVATE);
-                String number = sharedPreferences.getString("numberMom", "");
+                String number = sharedPreferences.getString("numberMom", "111");
                 Log.i("info", "Long");
                 //below code is for sending sms
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
@@ -150,7 +149,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             @Override
             public boolean onLongClick(View v) {
                 SharedPreferences sharedPreferences = getSharedPreferences("fileNameString", MODE_PRIVATE);
-                String number2 = sharedPreferences.getString("numberDad", "");
+                String number2 = sharedPreferences.getString("numberDad", "222");
                 Log.i("info", "Long");
                 //below code is for sending sms
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
@@ -222,7 +221,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         try {
             locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
-        } catch (SecurityException e) {
+        }
+        catch(SecurityException e) {
             e.printStackTrace();
         }
     }
@@ -231,180 +231,51 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     public void onLocationChanged(Location location) {
 
         Log.i("info", location.getLatitude() + "");
-        //new MyTask().execute();
 
-        Thread thread = new Thread(){
-            public void run(){
-                Looper.prepare();//Call looper.prepare()
-
-                Handler mHandler = new Handler() {
-                    public void handleMessage(Message msg) {
-                        Toast.makeText(getApplicationContext(), "Finco is Daddy", Toast.LENGTH_LONG);
-                        Log.i("info", "hi");
-                    }
-                };
-
-                Looper.loop();
-            }
-        };
-        thread.start();
-/*
-        new Handler(Looper.getMainLooper()).post(new Runnable() {
-            @Override
-            public void run() {
-                getAddress_andSendSms(location);
-            }
-        });
-        */
-
-        /*
-        runOnUiThread(new Runnable() {
-            public void run() {
-                getAddress_andSendSms(location);
-            }
-        });
-*/
-        /*
-        new Thread(new Runnable(){
-            @Override
-            public void run() {
-                getAddress_andSendSms(location);
-            }
-        }).start();
-*/
+        //below code is for sending sms
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+        SmsManager sms = SmsManager.getDefault();
+        String address = null;
+        try {
+            address = test(location);
+            Log.i("info", address);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sms.sendTextMessage(ph_no, null, "Need Help. Current Location: " + location.getLatitude() + ", " + location.getLongitude() + "Address=" + address, pi, null);
+        Toast.makeText(getApplicationContext(), "Message Sent successfully!",
+                Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-        Toast.makeText(MainActivity.this, "Please Give Permissions", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
     }
 
-    private void longClickAction(String person) {
+    private void longClickAction(String person){
         SharedPreferences sharedPreferences = getSharedPreferences("fileNameString", MODE_PRIVATE);
         ph_no = sharedPreferences.getString(person, "");
         Log.i("info", "Long");
         getLocation();
         Toast.makeText(getApplicationContext(), "Message Will Be Sent Shortly",
-                Toast.LENGTH_LONG).show();
+        Toast.LENGTH_LONG).show();
     }
 
-    public static void getAddressFromLocation(final double latitude, final double longitude, final Context context, final Handler handler) {
-        Thread thread = new Thread() {
-            @Override
-            public void run() {
-                Geocoder geocoder = new Geocoder(context, Locale.getDefault());
-                String result = null;
-                try {
-                    List<Address> addressList = geocoder.getFromLocation(latitude, longitude, 1);
-                    if (addressList != null && addressList.size() > 0) {
-                        Address address = addressList.get(0);
-                        StringBuilder sb = new StringBuilder();
-                        for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
-                            sb.append(address.getAddressLine(i)); //.append("\n");
-                        }
-                        sb.append(address.getLocality()).append("\n");
-                        sb.append(address.getPostalCode()).append("\n");
-                        sb.append(address.getCountryName());
-                        result = sb.toString();
-                    }
-                } catch (IOException e) {
-                    Log.e("Location Address Loader", "Unable connect to Geocoder", e);
-                } finally {
-                    Message message = Message.obtain();
-                    message.setTarget(handler);
-                    if (result != null) {
-                        message.what = 1;
-                        Bundle bundle = new Bundle();
-                        bundle.putString("address", result);
-                        message.setData(bundle);
-                    } else {
-                        message.what = 1;
-                        Bundle bundle = new Bundle();
-                        result = " Unable to get address for this location.";
-                        bundle.putString("address", result);
-                        message.setData(bundle);
-                    }
-                    message.sendToTarget();
-                }
-            }
-        };
-        thread.start();
+    public String test(Location location) throws IOException {
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude() , 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+        String city = addresses.get(0).getLocality();
+        String state = addresses.get(0).getAdminArea();
+        String country = addresses.get(0).getCountryName();
+        String postalCode = addresses.get(0).getPostalCode();
+        String knownName = addresses.get(0).getFeatureName();
+        Log.i("address", address);
+        return address;
     }
-
-    public void getAddress_andSendSms()
-    {
-        /*Location location = null;
-        location.setLatitude(27);
-        location.setLongitude(-122);
-        */
-        try
-        {
-            //URL url = new URL("https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=" + location.getLatitude() + "&longitude=" + location.getLongitude() + "&localityLanguage=en");
-            URL url = new URL("https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=" + "23" + "&longitude=" + "-123" + "&localityLanguage=en");
-            URLConnection urlcon=url.openConnection();
-            System.out.println("------------------------------------");
-
-            //get the inputstream of the open connection.
-            BufferedReader br = new BufferedReader(new InputStreamReader(urlcon.getInputStream()));
-            System.out.println(br.toString());
-            String i;
-            StringBuilder sb = new StringBuilder();
-            while ((i = br.readLine()) != null)
-            {
-                System.out.println(i);
-                sb.append(i);
-            }
-            System.out.println(sb.toString());
-            JSONObject json = new JSONObject(sb.toString());
-            String city = json.getString("city");
-            System.out.println(city);
-
-            //below code is for sending sms
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
-            SmsManager sms = SmsManager.getDefault();
-            //sms.sendTextMessage(ph_no, null, "Need Help. Current Location: " + location.getLatitude() + ", " + location.getLongitude() + " City Is:" + city, pi, null);
-            sms.sendTextMessage(ph_no, null, "Need Help. Current Location: "  + ", "  + " City Is:" + city, pi, null);
-            Toast.makeText(getApplicationContext(), "Message Sent successfully!",
-                    Toast.LENGTH_LONG).show();
-
-        }
-        catch (Exception e)
-        {
-            System.out.println(e);
-        }
-    }
-
-    private class GeocoderHandler extends Handler {
-        @Override
-        public void handleMessage(Message message) {
-            String locationAddress;
-            switch (message.what) {
-                case 1:
-                    Bundle bundle = message.getData();
-                    locationAddress = bundle.getString("address");
-                    break;
-                default:
-                    locationAddress = null;
-            }
-            Log.e("location Address=", locationAddress);
-        }
-    }
-
-    private class MyTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-            //String url = params[0];
-            getAddress_andSendSms();
-            return "abc";
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            // do something with result
-        }
-    }
-
 }
